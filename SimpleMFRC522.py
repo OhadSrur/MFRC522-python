295263872962c39f8813a8b49c3f6f87e02e5aa6
OhadSrur <ohadsrur@gmail.com>
2018-11-24 21:57:06 +1100

    Update SimpleMFRC522.py

--- SimpleMFRC522.py
+++ SimpleMFRC522.py
@@ -92,7 +92,7 @@ class SimpleMFRC522:
           n = n * 256 + uid[i]
       return n
     
-   def uid_to_hex(self, uid):
+  def uid_to_hex(self, uid):
       res = ''
       HEXA = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
       for i in range(0, 4):
